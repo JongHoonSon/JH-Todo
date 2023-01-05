2d8ce00aea4e850a18b4bba51e8183aa5810ca31
JongHoonSon <roka1684@gmail.com>
2023-01-06 01:56:57 +0900

    Feat: 로그아웃 구현

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,36 @@
+import styled from "styled-components";
+import { useState } from "react";
+import { useEffect } from "react";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  const [isLoggedIn, setIsLoggedIn] = useState<Boolean>(false);
+
+  useEffect(() => {
+    if (window.localStorage.getItem("jwt")) setIsLoggedIn(true);
+  }, []);
+
+  const handleLogoutButtonClick = () => {
+    window.localStorage.removeItem("jwt");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <Container>
+      {isLoggedIn && (
+        <Button onClick={handleLogoutButtonClick}>로그아웃</Button>
+      )}
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 50%;
+  height: 50%;
+  margin: auto;
+  background-color: yellowgreen;
+`;
+
+const Button = styled.button`
+  width: 100px;
+  height: 80px;
+`;
