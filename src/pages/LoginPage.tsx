2b9c20d62422596b71de32be1961a7317bc51de9
JongHoonSon <roka1684@gmail.com>
2023-01-06 01:37:16 +0900

    Feat: 로그인 구현

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,60 @@
 import { Helmet } from "react-helmet";
 import styled from "styled-components";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useRef } from "react";
+import { vaildateEmail, validatePassword } from "./../utils/validateForm";
+import axios from "axios";
 
 export const LoginPage = () => {
+  const [emailInputValue, setEmailInputValue] = useState<string>("");
+  const [passwordInputValue, setPasswordInputValue] = useState<string>("");
+  const [isButtonActive, setIsButtonActive] = useState<boolean>(false);
+  const submitButton = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (
+      vaildateEmail(emailInputValue) &&
+      validatePassword(passwordInputValue)
+    ) {
+      if (!isButtonActive) setIsButtonActive(true);
+    } else {
+      if (isButtonActive) setIsButtonActive(false);
+    }
+  }, [emailInputValue, passwordInputValue]);
+
+  useEffect(() => {
+    submitButton.current?.toggleAttribute("disabled");
+  }, [isButtonActive]);
+
+  const handleFormSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(e);
+
+    axios
+      .post("http://localhost:8080/users/login", {
+        email: emailInputValue,
+        password: passwordInputValue,
+      })
+      .then((res) => console.log(res.data));
+  };
+
   return (
     <Container>
       <Helmet>
         <title>로그인</title>
       </Helmet>
+      <form onSubmit={(e) => handleFormSubmit(e)}>
+        <input
+          value={emailInputValue}
+          onChange={(e) => setEmailInputValue(e.target.value)}
+        />
+        <input
+          value={passwordInputValue}
+          onChange={(e) => setPasswordInputValue(e.target.value)}
+        />
+        <input type="submit" ref={submitButton} disabled />
+      </form>
     </Container>
   );
 };
