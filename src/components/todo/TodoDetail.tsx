dc6c5795d52853cd213a81958a2950d3676b8402
JongHoonSon <roka1684@gmail.com>
2023-01-16 00:11:26 +0900

    Feat: selectedTodo의 id로 서버로부터 todo 정보를 받아오도록 수정

--- src/components/todo/TodoDetail.tsx
+++ src/components/todo/TodoDetail.tsx
@@ -1,23 +1,46 @@
 import styled from "styled-components";
 import { ITodo } from "../../types/todo";
+import { getTodoById } from "./../../api/todo/getTodoById";
+import { useQuery } from "@tanstack/react-query";
 
 interface TodoDetailProps {
   selectedTodo: ITodo | undefined;
 }
 
 export const TodoDetail = ({ selectedTodo }: TodoDetailProps) => {
+  const { data: todo, refetch } = useQuery<ITodo | undefined>(
+    ["getTodoById", `${selectedTodo?.id}`],
+    () =>
+      getTodoById(
+        selectedTodo === undefined
+          ? { todoId: "0" }
+          : { todoId: selectedTodo.id }
+      )
+  );
   return (
     <Container>
-      {selectedTodo === undefined ? (
+      {todo === undefined ? (
         "empty"
       ) : (
-        <TodoDetailContainer>
-          <span>{`제목 : ${selectedTodo.title}`}</span>
-          <span>{`내용 : ${selectedTodo.content}`}</span>
-          <span>{`id : ${selectedTodo.id}`}</span>
-          <span>{`작성일 : ${selectedTodo.createdAt}`}</span>
-          <span>{`수정일 : ${selectedTodo.updatedAt}`}</span>
-        </TodoDetailContainer>
+        <>
+          <TodoDetailContainer>
+            <span>{`제목 : ${todo.title}`}</span>
+            <span>{`내용 : ${todo.content}`}</span>
+            <span>{`id : ${todo.id}`}</span>
+            <span>{`작성일 : ${todo.createdAt}`}</span>
+            <span>{`수정일 : ${todo.updatedAt}`}</span>
+          </TodoDetailContainer>
+          <TodoEditFormContainer>
+            <CustomForm>
+              <label htmlFor="titleInput">제목</label>
+              <input type="text" id="titleInput" value={todo.title} />
+              <label htmlFor="contentInput">내용</label>
+              <input type="text" id="contentInput" value={todo.content} />
+              <input type="button" value="수정" />
+            </CustomForm>
+          </TodoEditFormContainer>
+          <TodoDeleteButton>삭제</TodoDeleteButton>
+        </>
       )}
     </Container>
   );
@@ -28,14 +51,37 @@ const Container = styled.div`
   height: 100%;
   margin: auto;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: green;
 `;
 
 const TodoDetailContainer = styled.div`
+  width: 50%;
+  height: 50%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const TodoEditFormContainer = styled.div`
+  width: 50%;
+  height: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CustomForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TodoDeleteButton = styled.button`
+  width: 50px;
+  height: 50px;
 `;
