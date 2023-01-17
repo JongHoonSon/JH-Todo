e2f5796202550bced0a35612f1adb07572168ebb
JongHoonSon <roka1684@gmail.com>
2023-01-17 17:58:44 +0900

    Feat: NotFoundPage 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { HomePageContainer } from "./containers/home/HomePageContainer";
 import { LoginPageContainer } from "./containers/auth/LoginPageContainer";
 import { JoinPageContainer } from "./containers/auth/JoinPageContainer";
 import { TodoPageContainer } from "./containers/todo/TodoPageContainer";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 export const Router = () => {
   return (
@@ -12,6 +13,7 @@ export const Router = () => {
         <Route path="/auth/join" element={<JoinPageContainer />} />
         <Route path="/auth/login" element={<LoginPageContainer />} />
         <Route path="/todo" element={<TodoPageContainer />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
