ebcf13d61b48270e6819caef8075cd7290cb5e1a
JongHoonSon <roka1684@gmail.com>
2023-01-18 23:06:54 +0900

    Feat: todo 삭제 이후 selectedTodo undefined 로 초기화

--- src/hooks/api/todo/useDeleteTodoMutation.ts
+++ src/hooks/api/todo/useDeleteTodoMutation.ts
@@ -1,8 +1,12 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useAppDispatch } from "../../../store/store";
 import { deleteTodo } from "../../../api/todo/deleteTodo";
+import { todoSlice } from "./../../../store/todoSlice";
 
 export const useDeleteTodoMutation = () => {
   const queryClient = useQueryClient();
+  const dispatch = useAppDispatch();
+  const { setSelectedTodo } = todoSlice.actions;
 
   return useMutation(deleteTodo, {
     onMutate: () => {
@@ -13,6 +17,7 @@ export const useDeleteTodoMutation = () => {
       console.log("data", data);
       queryClient.invalidateQueries({ queryKey: ["getTodos"] });
       queryClient.invalidateQueries({ queryKey: ["getTodoById"] });
+      dispatch(setSelectedTodo(undefined));
     },
     onError: (error) => {
       console.log("onError");
