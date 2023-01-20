4327d98f9569ab5b1aee30b9975f2e227845822f
JongHoonSon <roka1684@gmail.com>
2023-01-20 16:31:44 +0900

    Style: LoginPage style 적용

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -40,48 +40,78 @@ export const LoginPage = (): React.ReactElement => {
   });
 
   return (
-    <Container>
+    <>
       <Helmet>
         <title>로그인</title>
       </Helmet>
-      <form onSubmit={handleSubmit(handleLoginFormSubmit)}>
-        <label htmlFor="email">이메일</label>
-        <input
-          type="text"
-          id="email"
-          defaultValue=""
-          {...register("email", {
-            required: "이메일을 입력해 주세요.",
-            validate: vaildateEmail,
-          })}
-          placeholder="이메일"
-        />
-        {errors.email && <span>{errors.email.message}</span>}
-        <label htmlFor="password">비밀번호</label>
-        <input
-          type="password"
-          id="password"
-          defaultValue=""
-          {...register("password", {
-            required: "비밀번호를 입력해 주세요.",
-            validate: validatePassword,
-          })}
-          placeholder="비밀번호"
-        />
-        {errors.password && <span>{errors.password.message}</span>}
+      <Container>
+        <LoginForm onSubmit={handleSubmit(handleLoginFormSubmit)}>
+          <InputWrapper>
+            <label htmlFor="email">이메일</label>
+            <input
+              type="text"
+              id="email"
+              defaultValue=""
+              {...register("email", {
+                required: "이메일을 입력해 주세요.",
+                validate: vaildateEmail,
+              })}
+              placeholder="이메일"
+            />
+          </InputWrapper>
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+
+          <InputWrapper>
+            <label htmlFor="password">비밀번호</label>
+            <input
+              type="password"
+              id="password"
+              defaultValue=""
+              {...register("password", {
+                required: "비밀번호를 입력해 주세요.",
+                validate: validatePassword,
+              })}
+              placeholder="비밀번호"
+            />
+          </InputWrapper>
+          {errors.password && (
+            <ErrorMessage>{errors.password.message}</ErrorMessage>
+          )}
+          <input type="submit" disabled={!isValid} />
+        </LoginForm>
         {loginError && (
-          <span>아이디 또는 비밀번호 정보가 일치하지 않습니다.</span>
+          <ErrorMessage>
+            아이디 또는 비밀번호 정보가 일치하지 않습니다.
+          </ErrorMessage>
         )}
-        <input type="submit" disabled={!isValid} />
-      </form>
-      <Link to="/auth/join">회원가입</Link>
-    </Container>
+        <Link to="/auth/join">회원가입</Link>
+      </Container>
+    </>
   );
 };
 
 const Container = styled.div`
   width: 500px;
   height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   margin: auto;
-  background-color: skyblue;
+  border: 1px solid skyblue;
+  border-radius: 50px;
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InputWrapper = styled.div``;
+
+const ErrorMessage = styled.span`
+  font-size: 16px;
+  color: red;
 `;
