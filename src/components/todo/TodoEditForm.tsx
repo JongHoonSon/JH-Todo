492abe9d8f4b14242f5d7e1eea1af5da3d9b7400
JongHoonSon <roka1684@gmail.com>
2023-01-26 14:13:45 +0900

    Feat: TodoEditForm useForm으로 재구현

--- src/components/todo/TodoEditForm.tsx
+++ src/components/todo/TodoEditForm.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
-import { useState } from "react";
-import { ITodo } from "../../types/todoTypes";
+import { ITodo, TodoFormInputs } from "../../types/todoTypes";
 import { useUpdateTodoMutation } from "./../../hooks/api/todo/useUpdateTodoMutation";
 import { useGetTodoByIdQuery } from "../../hooks/api/todo/useGetTodoByIdQuery";
 import { useConfirm } from "material-ui-confirm";
 import { SubmitInput } from "../common/SubmitInput";
 import { Button } from "../common/Button";
+import { useForm } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form/dist/types";
+import { colors } from "./../../styles/colors";
 
 interface TodoEditFormProps {
   selectedTodo: ITodo | undefined;
@@ -17,50 +19,57 @@ export const TodoEditForm = ({
   handleTodoEditFormOpenChange,
 }: TodoEditFormProps) => {
   const { data: todo } = useGetTodoByIdQuery(selectedTodo);
-  const [newTitle, setNewTitle] = useState<string>(todo ? todo.title : "");
-  const [newContent, setNewContent] = useState<string>(
-    todo ? todo.content : ""
-  );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TodoFormInputs>();
   const updateTodoMutation = useUpdateTodoMutation();
   const confirm = useConfirm();
 
-  const handleTodoEditFormSumbit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const handleTodoEditFormSumbit: SubmitHandler<TodoFormInputs> = (data) => {
     confirm({ title: "수정하시겠습니까?" }).then(() => {
       updateTodoMutation.mutate({
         todoId: selectedTodo ? selectedTodo.id : "",
-        todoTitle: newTitle,
-        todoContent: newContent,
+        todoTitle: data.title,
+        todoContent: data.content,
       });
-      setNewTitle("");
-      setNewContent("");
       handleTodoEditFormOpenChange(false);
     });
   };
   return (
     <Container>
-      <TodoEditFormHeader>할 일 수정</TodoEditFormHeader>
-      <CustomForm onSubmit={handleTodoEditFormSumbit}>
+      <TodoFormHeader>할 일 수정</TodoFormHeader>
+      <TodoForm onSubmit={handleSubmit(handleTodoEditFormSumbit)}>
         <InputWrapper>
           <Label htmlFor="titleInput">제목</Label>
           <Input
             type="text"
             id="titleInput"
-            value={newTitle}
-            onChange={(e) => setNewTitle(e.target.value)}
+            {...register("title", {
+              value: todo?.title,
+              required: "제목을 입력해 주세요.",
+            })}
           />
         </InputWrapper>
+        <InputError>
+          <span>{errors.title && errors.title.message}</span>
+        </InputError>
 
         <InputWrapper>
           <Label htmlFor="contentInput">내용</Label>
           <Input
             type="text"
             id="contentInput"
-            value={newContent}
-            onChange={(e) => setNewContent(e.target.value)}
+            {...register("content", {
+              value: todo?.content,
+              required: "내용을 입력해 주세요.",
+            })}
           />
         </InputWrapper>
+        <InputError>
+          <span>{errors.content && errors.content.message}</span>
+        </InputError>
 
         <TodoOptionContainer>
           <SubmitInput type="submit" value="확인" />
@@ -68,7 +77,7 @@ export const TodoEditForm = ({
             취소
           </Button>
         </TodoOptionContainer>
-      </CustomForm>
+      </TodoForm>
     </Container>
   );
 };
@@ -88,23 +97,23 @@ const Container = styled.div`
   border-radius: 20px;
 `;
 
-const TodoEditFormHeader = styled.div`
+const TodoFormHeader = styled.div`
   width: 90%;
   height: 20%;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 18px;
+  font-size: 28px;
   font-weight: 600;
   color: ${(props) => props.theme.textColor_primary};
 `;
 
-const CustomForm = styled.form`
+const TodoForm = styled.form`
   width: 100%;
-  height: 50%;
+  height: 80%;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
 `;
 
@@ -126,6 +135,19 @@ const Input = styled.input`
   width: 300px;
   height: 40px;
   padding: 5px 10px;
+`;
+
+const InputError = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+
+  color: ${colors.ERROR};
+  font-size: 14px;
+
+  height: 50px;
+  margin-bottom: 5px;
 `;
 
 const TodoOptionContainer = styled.div`
