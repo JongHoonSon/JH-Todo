5f933775847d449ad67b23c7ccd20c63269c661e
JongHoonSon <roka1684@gmail.com>
2023-01-27 21:35:01 +0900

    Feat: 로그아웃 버튼에 confirm 적용

--- src/components/header/LogoutButton.tsx
+++ src/components/header/LogoutButton.tsx
@@ -1,13 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import { checkUserLoggedIn } from "./../../utils/checkUserLoggedIn";
 import { Button } from "./../common/Button";
+import { useConfirm } from "material-ui-confirm";
 
 export const LogoutButton = () => {
   const navigate = useNavigate();
+  const confirm = useConfirm();
   const isUserLoggedIn = checkUserLoggedIn();
   const handleLogoutButtonClick = () => {
-    window.localStorage.removeItem("jwt");
-    navigate("/");
+    confirm({ title: "로그아웃 하시겠습니까?" }).then(() => {
+      window.localStorage.removeItem("jwt");
+      navigate("/");
+    });
   };
 
   return isUserLoggedIn ? (
